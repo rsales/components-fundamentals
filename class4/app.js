a6e39b70bfb2872432afb2e18c9ba5939cd4371a
Rafael Sales <rafael.sales93@hotmail.com>
2020-03-23 22:26:40 -0300

    first commit

---
+++ class4/app.js
@@ -0,0 +1,22 @@
+Vue.component('plan-picker', {
+  template: '#plan-picker-template',
+  data() {
+    return {
+      plans: ['The Single', 'The Curious', 'The Addict'],
+    }
+  }
+})
+
+Vue.component('plan', {
+  template: '#plan-template',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  }
+})
+
+new Vue({
+  el: '#app',
+})
---
+++ class5/app.js
@@ -0,0 +1,28 @@
+let PlanComponent = {
+  template: '#plan-template',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  }
+}
+
+let PlanPickerComponent = {
+  template: '#plan-picker-template',
+  components: {
+    plan: PlanComponent,
+  },
+  data() {
+    return {
+      plans: ['The Single', 'The Curious', 'The Addict'],
+    }
+  }
+}
+
+new Vue({
+  el: '#app',
+  components: {
+    'plan-picker': PlanPickerComponent,
+  },
+})
